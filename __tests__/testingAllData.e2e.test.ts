4bf8e9abb4c392f8c302e326e06663bb582f7bd7
Lsir4ik <lazarev171193@mail.ru>
2024-09-20 20:48:46 +0400

    add tests

--- __tests__/testingAllData.e2e.test.ts
+++ __tests__/testingAllData.e2e.test.ts
@@ -1,5 +1,14 @@
-describe('/testing/all-data', () => {
-    it('DELETE, should remove all data, status 204', () => {
+import {agent} from "supertest";
+import {app} from "../src/app";
+import {request} from "node:http";
+import {HTTPStatusCodesEnum, SETTINGS} from "../src/settings";
 
+const req = agent(app)
+
+describe('/testing/all-data', () => {
+    it('DELETE, should remove all data, status 204', async () => {
+        await req.delete(SETTINGS.PATH.TESTING)
+            .expect(HTTPStatusCodesEnum.No_Content_204)
+        await req.get(SETTINGS.PATH.BLOGS).
     })
 })
