edf6c2cda969d3cfd025a5bde9e94becc8361f78
Lsir4ik <lazarev171193@mail.ru>
2024-09-22 14:11:49 +0400

    refactor input validation

--- src/middlewares/validation/input.validation.middleware.ts
+++ src/middlewares/validation/input.validation.middleware.ts
@@ -1,16 +1,19 @@
 import {NextFunction, Request, Response} from "express";
-import {validationResult} from "express-validator";
+import {ValidationError, validationResult} from "express-validator";
 import {HTTPStatusCodesEnum} from "../../settings";
+import {FieldValidationError} from "express-validator/lib/base";
 
 export const inputValidationMiddleware = (req: Request,res :Response, next:NextFunction) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         console.log(errors)
         res.status(HTTPStatusCodesEnum.Bad_Request_400).json({
-            errorsMessages: errors.array({onlyFirstError: true}).map(error => {
+            errorsMessages:( errors
+                .array({onlyFirstError: true}) as FieldValidationError[])
+                .map((error) => {
                 return {
                     message: error.msg,
-                    field: error.type // TODO как достать path?
+                    field: error.path
                 }
 
             })
