3632b80a5be57d625bae16036edb881550a6a9d0
Lsir4ik <lazarev171193@mail.ru>
2024-10-23 21:44:31 +0400

    hw8 started

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,19 @@ import {userRouter} from "./users/users.router";
 import {routerPaths} from "./common/path/path";
 import {HttpStatusCodes} from "./common/types/httpsStatusCodes";
 import {commentsRouter} from "./comments/comments.router";
+import cookieParser from "cookie-parser";
+
 
 export const app = express();
 
-// Middlewares
-app.use(express.json()); // body и query в реквестах
+app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
-    res.status(HttpStatusCodes.No_Content_204).send("7th hometask already!")
-})
-app.use(routerPaths.testing,testingRouter);
+    res.status(HttpStatusCodes.No_Content_204).send("8th hometask already!")
+});
+app.use(routerPaths.testing, testingRouter);
 app.use(routerPaths.common, authRouter);
 app.use(routerPaths.blogs, blogsRouter);
 app.use(routerPaths.posts, postsRouter);
